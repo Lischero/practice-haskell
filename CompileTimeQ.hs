08a341abea619c326422e09017046ba73f98f2ed
MichaeGon <lodnix@gmail.com>
2016-09-29 22:28:50 +0900

    test

---
+++ CompileTimeQ.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE TemplateHaskell, QuasiQuotes #-}
+
+module CompileTimeQ
+    ( playQ
+    ) where
+
+import Language.Haskell.TH
+import System.Process
+
+import CompileTimeQ.Core
+
+playQ :: DecsQ
+playQ = runIO ioAction >> [d| dummy = undefined |]
+    where
+        ioAction = createProcess playOnMac
---
+++ CompileTimeQ/Core.hs
@@ -0,0 +1,41 @@
+module CompileTimeQ.Core
+    ( playOnMac
+    , brewConfig
+    , play
+    ) where
+
+import System.Process
+
+playOnMac :: CreateProcess
+playOnMac = shell . unlines $ brewConfig' ++ play'
+
+brewConfig' :: [String]
+brewConfig' = [
+    "ytdl=youtube-dl",
+    "res=`brew list | grep ${ytdl}`",
+    "if [ \"${res}\" != ${ytdl} ]; then",
+    "echo 'configure...'",
+    "brew install ${ytdl}",
+    {-
+    "else",
+    "youtube-dl -U",
+    "echo 'configure...'",
+    -}
+    "fi",
+    "mpr=mplayer",
+    "res2=`brew list | grep ${mpr}`",
+    "if [ \"${res2}\" != ${mpr} ]; then",
+    "brew install ${mpr}",
+    "fi",
+    "echo 'done'"]
+
+brewConfig :: CreateProcess
+brewConfig = shell . unlines $ brewConfig'
+
+play' :: [String]
+play' = [
+    "geko='https://www.youtube.com/watch?v=OxXzOA784X8'",
+    "youtube-dl ${geko} -q -o - | mplayer - -novideo"]
+
+play :: CreateProcess
+play = shell . unlines $ play'
