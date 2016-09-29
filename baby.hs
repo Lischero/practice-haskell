5b61b78a9c60b4909cbc551c1f3f05922f725878
MichaeGon <lodnix@gmail.com>
2016-09-29 22:49:59 +0900

    stack

--- baby.hs
+++ baby.hs
@@ -7,3 +7,5 @@ conanO'Brien = "It's a-me, conan O'Brien!"
 boomBang xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs , odd x]
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 answerTriangles = [ (a,b,c) | c <- [1..10], a <- [1..c], b <- [1..a], a+b+c == 24, a^2 + b^2 == c^2 ]
+
+main = putStrLn "hello, world"
